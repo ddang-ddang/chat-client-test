c7b81f5b09dd25139aa4aea7d10b5b50742bd34b
moto4321 <moto432195@gmail.com>
2022-05-12 17:01:12 +0900

    [feat] 채팅 클라이언트 테스팅용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ChatPage from './Pages/ChatPage';
+import LandingPage from './Pages/LandingPage';
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <link
+          href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+          rel="stylesheet"
+          id="bootstrap-css"
+        />
+        <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+            type="text/css"
+            rel="stylesheet"
+        />
+        <link href="/style.css" type="text/css" rel="stylesheet" />
+        <script
+            src="https://cdn.socket.io/3.1.3/socket.io.min.js"
+            integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh"
+            crossorigin="anonymous"
+        ></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      </Helmet>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />}/>
+          <Route exact path="/chat" element={<ChatPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>
+      <a href='/chat'><div>마을 채팅방 가기</div></a>
+    </div>
+  )
+}
+
+export default LandingPage
